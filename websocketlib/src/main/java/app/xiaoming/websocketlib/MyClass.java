6254c3ea32a1fc4551f83d1ac2fe05b51331fcb8
朱明仁 <mingren@cecelive.com>
2019-04-15 16:20:07 +0800

    Update websocketlib

--- websocketlib/src/main/java/app/xiaoming/websocketlib/MyClass.java
+++ websocketlib/src/main/java/app/xiaoming/websocketlib/MyClass.java
@@ -2,7 +2,11 @@ package app.xiaoming.websocketlib;
 
 public class MyClass {
     
-    public static void main(String[] args) {
+    public static final void main(String[] args) {
+        sop("Hello");
+    }
     
+    public static void sop(String ss) {
+        System.out.println(ss);
     }
 }
